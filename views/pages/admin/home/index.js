80f5abc60e7a3591f35fff7ba8bff3b98dae51e1
[SW5] 정다희01~ <tlwidacot125@gmail.com>
2023-07-07 20:13:50 +0900

    feat: admin-home 헤더, 푸터 추가

--- views/pages/admin/home/index.js
+++ views/pages/admin/home/index.js
@@ -1,12 +1,37 @@
-let mainNav = document.getElementById("mainNav");
+const categories = [
+    {
+      "_id": "1",
+      "category": "사료"
+    },
+    {
+      "_id": "2",
+      "category": "외출용품"
+    },
+    {
+      "_id": "3",
+      "category": "장난감"
+    },
+    {
+      "_id": "4",
+      "category": "고양이 의류"
+    },
+    {
+      "_id": "5",
+      "category": "건강관리"
+    }
+  ];
 
-let tempNavData = [{category: 'About'}, {category : '사료'}, {category : '간식'}, {category : '장난감'},{category : '모래/화장실'},]
+// 카테고리 불러오기 함수
+let currentCategoryIndex = 0;
+const mainNav = document.querySelector('.mainNav');
 
-// index
-let mainNavLi = '';
-for(let i = 0; i < tempNavData.length; i++) {
-    mainNavLi += `<li><a href = '' class = "navMenus">${tempNavData[i].category}</a></li>`;
+function showCategories() {
+    const categoryElements = categories.map(category => `<li><a href="">${category.category}</a></li>
+    `);
+    mainNav.innerHTML = `<ul>${categoryElements.join('')}<ul>`;
+
+    console.log(categoryElements);
 }
 
-console.log(mainNavLi);
-mainNav.innerHTML = `<ul>${mainNavLi}</ul>`;
+//페이지 로드 시 카테고리 보여주기
+showCategories();
