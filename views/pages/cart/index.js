96d24ff761d277ab4a1f479003ca8098c8ff4d92
[SW5] 안보란 <boransama@gmail.com>
2023-07-07 17:59:13 +0900

    feat: 복수개 상품 추가 기능, 장바구니 전체 비우기 기능 추가

--- views/pages/cart/index.js
+++ views/pages/cart/index.js
@@ -1,62 +1,84 @@
+const cartList = document.querySelector(".cart_list");
+const idList = []; 
+
 // 로컬스토리지의 key(id)만 배열로 가져옴
-const idList = [];
 for (let i = 0; i < localStorage.length; ++i) {
   idList.push(localStorage.key(i));
 }
+// 각 상품을 id로 가져와서 Value를 배열에 넣기
+// 배열을 페이지의 innerHTML에 넣어 요소로 추가
+for (let i = 0; i < idList.length; ++i) {
+  const parsed = JSON.parse(localStorage.getItem(idList[i]));
+  parsed.index = i + 1;    // 일련번호 지정
+  if (!parsed.qty) parsed.qty = 1;    // qty속성이 없을 경우에만 qty속성 추가
+  
+  const content = `
+    <div class="product_wrap">
+      <span class="product_index">${parsed.index}</span>
+      <div>
+        <div class="product_thumbnail">
+          <img src="${parsed.repImgUrl}" alt="thumbnail"/>
+        </div>
+        <span>${parsed.name}</span>
+        <span>
+          <button class="qty_down" onclick="qtyDown(event)">-</button>
+          <span class="product_qty">${parsed.qty}</span>
+          <button class="qty_up" onclick="qtyUp(event)">+</button>
+        </span>
+        <span class="product_price">${parsed.price}</span>
+        <button class="delete_each" onclick="deleteEach(event)">❌</button>
+      </div>
+    </div>
+  `;
 
-// 각 상품 id로 API에서 상품정보를 얻어와서 장바구니에 추가
-// idList배열의 길이만큼 반복해서 추가해줘야 함
-
-if (localStorage.getItem('상품 id(sfsdf)')) {
-  let productData = '';
-  productData = JSON.parse(localStorage.getItem(productData));
-} else {
-  console.log('localStorage is empty.');
+  const newLi = document.createElement("li");
+  newLi.innerHTML = content;
+  cartList.appendChild(newLi);
 }
 
-// querySelectorAll로 고치기
-const qtyDownBtn = document.querySelector(".qty_down");
-const qtyUpBtn = document.querySelector(".qty_up");
-const deleteEachBtn = document.querySelector('.delete_each');
+const qtyUpBtns = document.querySelectorAll(".qty_up");
+const qtyDownBtns = document.querySelectorAll(".qty_down");
+const qtyElements = document.querySelectorAll('.product_qty');
+const deleteEachBtns = document.querySelectorAll('.delete_each');
 const deleteAllBtn = document.querySelector('.delete_all');
 
-// (이슈1/2) 수량은 어떻게 보존하는가? 다른 페이지 갔다가 오면 1이 되나?
-// 만들어 보고 동작 이상하면 이슈 올리기
-
-// 수량 변경 함수 구현하기
-
-const productQty = document.querySelector('.product_qty');
-
-function qtyDown() {
-  if (productQty.innerText === "1") return;
-  else productQty.innerText = Number(productQty.innerText) - 1;
+function qtyUp(event) {
+  const index = Number(event.target.parentElement.parentElement.previousSibling.previousSibling.innerText) - 1;
+  if (qtyElements[index].innerText === "9") {
+    alert("최대 구매 수량은 9개입니다.");
+  } else {
+    qtyElements[index].innerText = Number(qtyElements[index].innerText) + 1;
+    //=====로컬스토리지의 qty도 함께 수정되어야 함
+  }
 }
 
-function qtyUp() {
-  if (productQty.innerText === "99") alert("최대 구매 수량은 99개입니다.");
-  else productQty.innerText = Number(productQty.innerText) + 1;
+function qtyDown(event) {
+  const index = Number(event.target.parentElement.parentElement.previousSibling.previousSibling.innerText) - 1;
+  if (qtyElements[index].innerText === "1") {
+    return;
+  } else {
+    qtyElements[index].innerText = Number(qtyElements[index].innerText) - 1;
+    //=====로컬스토리지의 qty도 함께 수정되어야 함
+  }
 }
-
-// 로컬스토리지에서도 삭제하는 기능 구현해야 함
-// id배열을 filter로 !== 조건 걸어서 업데이트하기
 
 function deleteEach(event) {
-  // event.target은 deleteEachBtn임
-  const li = event.target.parentElement.parentElement;
+  // event.target은 deleteEachBtns임
+  const li = event.target.parentElement.parentElement.parentElement;
   li.remove();
+  // localStorage.removeItem();
+  //=======삭제할 제품의 id값을 알아야 함
   alert('삭제되었습니다.');
 }
 
-// (이슈2/2) 이것 때문에 async await를 쓰는 게 맞나...?
-
-async function deleteAll() {
-  await alert("장바구니를 비우시겠습니까?")
-  // 팝업에서 확인 누르면 로컬스토리지를 싹 비우고
-  // 취소누르면 아무것도 안 하고 return;하는 로직 짜기
-  // https://devsp.tistory.com/24
+function deleteAll() {
+  const userRes = confirm("장바구니를 비우시겠습니까?");
+  if (userRes === false) {
+    return;
+  } else {
+    localStorage.clear();
+    cartList.innerHTML = '';
+  }
 }
 
-qtyDownBtn.addEventListener('click', qtyDown);
-qtyUpBtn.addEventListener('click', qtyUp);
-deleteEachBtn.addEventListener('click', deleteEach);
 deleteAllBtn.addEventListener('click', deleteAll);
