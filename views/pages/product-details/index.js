d9c94612782cbb79516b344fe3448076db0ec452
[SW5] 안보란 <boransama@gmail.com>
2023-07-10 02:32:06 +0900

    fix: 상품상세에서 qty속성을 넣어 장바구니에 이미 담긴 상품도 더 담을 수 있게 수정

--- views/pages/product-details/index.js
+++ views/pages/product-details/index.js
@@ -1,10 +1,5 @@
-// function test() {
-//   location.href = '/product-details?id=64a8b5c760e6ded9c555e247'; // 이 주소로 이동
-// }
-
-console.log(location.search);
-//product-details?64a8b5c760e6ded9c555e247
-
+const urlParams = new URLSearchParams(window.location.search); // http://localhost:3000/product-details/?id=64a8b5c760e6ded9c555e247
+const id = urlParams.get('id');
 const infoBox = document.querySelector('#info_box');
 let productData = {};
 
@@ -16,6 +11,7 @@ async function getProductData(id) {
     });
 
     productData = await response.json();
+    productData.qty = 1;
 
     infoBox.innerHTML = `
       <p>${productData.name}</p>
@@ -29,29 +25,30 @@ async function getProductData(id) {
     //thumbnail.src = `${productData.repImgUrl}`;
 
     const addToCartBtn = document.querySelector('#addToCartButton');
-    addToCartBtn.addEventListener('click', test);
+    addToCartBtn.addEventListener('click', addToCart);
   } catch (err) {
     console.log(err);
   }
 }
 
-getProductData('64a8b5c760e6ded9c555e247');
+getProductData(id);
 
 function addToCart() {
-  // 로컬스토리지가 비어있으면 현재 상품을 추가
   if (localStorage.getItem('meowStoreCart') === null) {
     localStorage.setItem('meowStoreCart', JSON.stringify([productData]));
+    alert(`'${productData.name}'이(가) 장바구니에 추가되었습니다.`);
     return;
   }
 
-  // 로컬스토리지에 meowStoreCart가 있다면, 이 상품이 포함되어있는지 체크
-  const prevCartData = JSON.parse(localStorage.getItem('meowStoreCart'));
+  // 로컬스토리지에 meowStoreCart가 있다면, 이 상품이 들어가있는지 체크
+  let savedCartData = JSON.parse(localStorage.getItem('meowStoreCart'));
+
   const isAlreadyIn = () => {
     let bool = false;
-    const count = prevCartData.length;
+    const count = savedCartData.length;
     for (let i = 0; i < count; ++i) {
-      const curCartData = prevCartData[i];
-      if (productData._id === curCartData._id) {
+      const currentCartData = savedCartData[i];
+      if (productData._id === currentCartData._id) {
         bool = true;
         break;
       }
@@ -59,15 +56,21 @@ function addToCart() {
     return bool;
   };
 
-  // 이미 담겨있으면 수량 추가하기. map으로 아이디 찾아서 수량. 수량속성을 이 페이지에서 미리 넣어서 넘기기.
-
   if (isAlreadyIn()) {
-    alert(`이미 장바구니에 담겨진 상품입니다.`);
+    const userConfirm = confirm(
+      '이미 장바구니에 담겨진 상품입니다. 1개 더 추가하시겠습니까?',
+    );
+    if (userConfirm) {
+      savedCartData = savedCartData.map((o) => {
+        if (o._id === productData._id && o.qty < 9) o.qty += 1;
+        return o;
+      });
+      localStorage.setItem('meowStoreCart', JSON.stringify(savedCartData));
+    }
     return;
   }
-  // 상품이 들어있지 않으면 로컬스토리지에 추가
-  localStorage.removeItem('meowStoreCart');
-  prevCartData.push(productData);
-  localStorage.setItem('meowStoreCart', JSON.stringify(prevCartData));
+  // 이 상품이 장바구니에 담겨있지 않다면 추가
+  savedCartData.push(productData);
+  localStorage.setItem('meowStoreCart', JSON.stringify(savedCartData));
   alert(`'${productData.name}'이(가) 장바구니에 추가되었습니다.`);
 }
