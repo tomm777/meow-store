0fd1f7aadbdf3190a2d45c72dccfb53b67b41c3a
haeun.choi <haeunchoi.dev@gmail.com>
2023-07-11 16:38:30 +0900

    feat: header/footer 메인 화면에서 분리

---
+++ views/components/footer.js
@@ -0,0 +1,10 @@
+const footer = document.querySelector('footer');
+
+footer.innerHTML = `
+<div class="info">
+        <p>회사명 : 야옹잡화점</p>
+        <p>대표이메일 : meowstore@moewmoew.com</p>
+        <p>주소: 서울특별시 성동구 성수2가제3동 광나루로6길 49 성수낙낙</p>
+      </div>
+      <p class="copyright">Copyright © meowstore. all right reserved</p>
+`;
