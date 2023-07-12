9fe4af5137bd4d459d13cf2609cd1cfc9a23ff93
[SW5] 김기범01~ <tbbk1404@gmail.com>
2023-07-12 19:52:09 +0900

    feat:회원가입 페이지 API연동, 레이아웃 수정

--- views/pages/mypage/modify-info/index.js
+++ views/pages/mypage/modify-info/index.js
@@ -4,6 +4,18 @@ const emailInput = document.querySelector('#email');
 const phoneNumberInput = document.querySelector('#phone-number');
 const addressDetailInput = document.querySelector('#address-detail-input');
 // const passwordCheck = document.querySelector('#password-check');
+const addressZipCode = document.querySelector('#address-zip-input');
+const addressArea = document.querySelector('.address-area');
+const token =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NGFlM2VmMTllYzgyOGJiMWI4NmM5ZDMiLCJyb2xlIjoidXNlciIsImlhdCI6MTY4OTE0NDA0OH0.bDdFp-Xcc1f60impCwSYHS6xdnlBC-96jifAzFn2sxw';
+fetch('/api/user/mypage/64ae3ef19ec828bb1b86c9d3', {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `bearer ${token}`,
+  },
+})
+  .then((item) => item.json())
+  .then((result) => console.log(result));
 
 cansleButton.addEventListener('click', function () {
   window.location.href = '/mypage';
@@ -14,6 +26,7 @@ window.onload = function () {
   emailInput.value = 'Test@gmail.com';
   phoneNumberInput.value = '01012341234';
   addressDetailInput.value = '무슨아파트 10동';
+  addressZipCode.value = '25234';
   // 도로명 주소 가져오기
   document
     .querySelector('.modify-address')
