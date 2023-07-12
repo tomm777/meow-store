5cd47bc1b5c20d167ab76c7d61e8ee86cb2b2ed8
[SW5] 최하은 <haeunchoi.dev@gmail.com>
2023-07-12 22:06:24 +0900

    feat: user api 추가

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,6 +25,49 @@ class UserController {
 
   async getUser(req, res) {
     try {
+      const currentUserInfo = await UserService.getUserInfo(req.currentUserId);
+      res.status(200).json(currentUserInfo);
+    } catch (error) {
+      res
+        .status(error.status || 500)
+        .json({ success: false, message: error.message });
+    }
+  }
+
+  async updateUser(req, res) {
+    try {
+      const { password, name, address } = req.body;
+      const updatedUser = await UserService.updatedUserInfo(
+        req.currentUserId,
+        req.body.currentPassword,
+        {
+          password,
+          name,
+          address,
+        },
+      );
+      res.status(200).json({ updatedUser });
+    } catch (error) {
+      res
+        .status(error.status || 400)
+        .json({ success: false, message: error.message });
+    }
+  }
+
+  async deleteUser(req, res) {
+    try {
+      //TODO: 유저삭제시 관련 데이터 지우기
+      await UserService.deleteUser(req.currentUserId);
+      res.status(204).end();
+    } catch (error) {
+      res
+        .status(error.status || 500)
+        .json({ success: false, message: error.message });
+    }
+  }
+
+  async getUserByAdmin(req, res) {
+    try {
       const { userId } = req.params;
       const currentUserInfo = await UserService.getUserInfo(userId);
       res.status(200).json(currentUserInfo);
@@ -35,7 +78,7 @@ class UserController {
     }
   }
 
-  async updateUser(req, res) {
+  async updateUserByAdmin(req, res) {
     try {
       const { userId } = req.params;
       const { password, name, address } = req.body;
@@ -56,7 +99,7 @@ class UserController {
     }
   }
 
-  async deleteUser(req, res) {
+  async deleteUserByAdmin(req, res) {
     try {
       const { userId } = req.params;
       //TODO: 유저삭제시 관련 데이터 지우기
